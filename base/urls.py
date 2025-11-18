9d2d1e74785973f14337e62a333ff5d0dd09791c
punithm5 <punithlokesh42@gmail.com>
2025-11-19 02:17:59 +0530

    19/11/25

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home, name = 'home'),
-    path('room_page/<str:pk>/', views.room, name = 'room')
+    path('', views.home, name = 'homeURL'),
+    path('room_page/<str:pk>/', views.room, name = 'roomURL')
 ]
--- base/views.py
+++ base/views.py
@@ -11,6 +11,8 @@ rooms = Room.objects.all()
 
 def home(request):
     return render(request, 'base/home.html', {'rooms' : rooms})
+    # return render(http://127.0.0.1:8000/, 'templates/base/home.html', rooms)
+    # Together combined -> http://127.0.0.1:8000/templates/base/home.html
 
 def room(request, pk):
     room = Room.objects.get(id = pk)
@@ -22,4 +24,6 @@ def room(request, pk):
     context = {'room' : room}
     return render(request, 'base/room.html', context)
 
-# Create your views here.
+def CreateRoom(request):
+    context = {}
+    return render(request, 'base/room_form.html', context)
