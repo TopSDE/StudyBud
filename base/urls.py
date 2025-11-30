d657b5c13e143190c846cf000dbd5e5a478d40e8
punithm5 <punithlokesh42@gmail.com>
2025-11-30 17:45:19 +0530

    30/11/25

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.home, name = 'homeURL'),
     path('room_page/<str:pk>/', views.room, name = 'roomURL'),
-    path('create-room/', views.CreateRoom, name = "create-roomURL")
+    path('create-room/', views.CreateRoom, name = "create-roomURL"),
+    path('update-room/<str:pk>/', views.UpdateRoom, name = "update-roomURL"),
+    path('delete-room/<str:pk>/', views.DeleteRoom, name = "delete-roomURL")
 ]
--- base/views.py
+++ base/views.py
@@ -13,13 +13,13 @@ def home(request):
     rooms = Room.objects.all()
     print("Rooms:", rooms)
     return render(request, 'base/home.html', {'rooms' : rooms})
-    # return render(http://127.0.0.1:8000/, 'templates/base/home.html', rooms)
+    # return render("", 'templates/base/home.html', rooms)
     # Together combined -> http://127.0.0.1:8000/templates/base/home.html
     # Djago first looks for Project(base)/Templates folder, and since 'APP_DIRS': True in
     # settings.py, it then look for base/templates/base folder
 
 def room(request, pk):
-    room = Room.objects.get(id = pk)
+    room = Room.objects.get(id=pk)
     # room = None
     # for i in rooms:
     #     if i['id'] == int(pk):
@@ -33,9 +33,45 @@ def CreateRoom(request):
 
     if request.method == 'POST':
         form = RoomForm(request.POST)
+        # request.POST = new data submitted by user.
+        # instance=room = old data from the database.
+
+        # Django now knows:
+
+        # ✔ merge the new POST data
+        # ✔ with the old room instance
+        # ✔ validate the form
+        # ✔ save the updated record instead of creating new one
+
+        if form.is_valid():
+            form.save()
+            return redirect('homeURL')
+
+    context = {'form' : form}
+    # print('This is ', request.path)
+    return render(request, 'base/room_form.html', context)
+
+def UpdateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    # instance - “Load this form with the existing values of this room object.”
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        # “Update this specific room using the new POST data.”
         if form.is_valid():
             form.save()
             return redirect('homeURL')
 
     context = {'form' : form}
     return render(request, 'base/room_form.html', context)
+
+def DeleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'obj' : room}
+
+    if request.method == "POST":
+        room.delete()
+        return redirect('homeURL')
+
+    return render(request, 'base/delete.html', context)
